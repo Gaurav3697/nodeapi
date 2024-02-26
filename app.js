8bf4cf85e9d91e5d8485078f249143c230b6d798
Gaurav3697 <gm4063420@gmail.com>
2024-02-26 20:28:10 +0545

    changed cors position

--- app.js
+++ app.js
@@ -6,38 +6,30 @@ import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middlewares/error.js";
 import cors from "cors";
 
+
 config({
   path: "./data/config.env",
 });
 
+//Using Middleware
 export const app = express();
-app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.FRONTEND_URL
-      ? [process.env.FRONTEND_URL]
-      : "https://react-todo-8k5vabyut-gauravs-projects-3d02f3e1.vercel.app/",
+    ? [process.env.FRONTEND_URL]
+    : "https://react-todo-8k5vabyut-gauravs-projects-3d02f3e1.vercel.app/",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
-);
-
-//Using Middleware
-app.use(express.json());
-app.use("/api/v1/users", userRoutes); //It tells that in all the user's routes /users is already present
-app.use("/api/v1/task", taskRoutes);
-
-app.get("/", (req, res) => {
-  res.send("Nice working");
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", "true");
-  res.setHeader("Access-Control-Max-Age", "1800");
-  res.setHeader("Access-Control-Allow-Headers", "content-type");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "PUT, POST, GET, DELETE, PATCH, OPTIONS"
   );
-});
-
-//using Middleware
-app.use(errorMiddleware);
+  app.use(cookieParser());
+  app.use(errorMiddleware);
+  app.use(express.json());
+  app.use("/api/v1/users", userRoutes); //It tells that in all the user's routes /users is already present
+  app.use("/api/v1/task", taskRoutes);
+  
+// route
+  app.get("/",(req,res)=>{
+    res.send("Nice working");
+  })
+  
--- controllers/user.js
+++ controllers/user.js
@@ -5,15 +5,6 @@ import ErrorHandler from "../middlewares/error.js";
 
 export const getMyProfile = async (req, res,next) => {
     try {
-        res.send("Nice working");
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Credentials", "true");
-        res.setHeader("Access-Control-Max-Age", "1800");
-        res.setHeader("Access-Control-Allow-Headers", "content-type");
-        res.setHeader(
-          "Access-Control-Allow-Methods",
-          "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-        );
         res.status(200).json({
             success: true,
             user: req.user,
@@ -25,15 +16,6 @@ export const getMyProfile = async (req, res,next) => {
 
 export const login = async (req, res, next) => {
     try {
-        res.send("Nice working");
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Credentials", "true");
-        res.setHeader("Access-Control-Max-Age", "1800");
-        res.setHeader("Access-Control-Allow-Headers", "content-type");
-        res.setHeader(
-          "Access-Control-Allow-Methods",
-          "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-        );
         const { email, password } = req.body;
         const user = await User.findOne({ email }).select("+password");
         if (!user) return next(new ErrorHandler("Invalid Email or password", 400));
@@ -49,15 +31,6 @@ export const login = async (req, res, next) => {
 };
 export const logout = async (req, res) => {
     try {
-        res.send("Nice working");
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Credentials", "true");
-        res.setHeader("Access-Control-Max-Age", "1800");
-        res.setHeader("Access-Control-Allow-Headers", "content-type");
-        res.setHeader(
-          "Access-Control-Allow-Methods",
-          "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-        );
         res
         .status(200)
         .cookie("token", "", {
@@ -73,15 +46,6 @@ export const logout = async (req, res) => {
 
 export const register = async (req, res , next) => {
     try {
-        res.send("Nice working");
-        res.setHeader("Access-Control-Allow-Origin", "*");
-        res.setHeader("Access-Control-Allow-Credentials", "true");
-        res.setHeader("Access-Control-Max-Age", "1800");
-        res.setHeader("Access-Control-Allow-Headers", "content-type");
-        res.setHeader(
-          "Access-Control-Allow-Methods",
-          "PUT, POST, GET, DELETE, PATCH, OPTIONS"
-        );
         const { name, email, password } = req.body;
         let user = await User.findOne({ email });
         if (user) {
